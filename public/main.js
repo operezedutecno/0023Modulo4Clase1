5ac0d379df36e029e6bfc50060e8fc10f4c25705
Osman Pérez <operez@edutecno.com>
2023-05-08 21:12:07 -0400

    Clase de Babel

--- public/main.js
+++ public/main.js
@@ -1,8 +1,9 @@
+"use strict";
+
 var nombre = "Luis";
 var nombre = "Pedro";
-var apellido = "Jimenez";
 
-const porcentaje = 40;
+var porcentaje = 40;
 // const porcentaje = 50;
 
 // console.log("porcentaje", porcentaje);
@@ -10,27 +11,24 @@ const porcentaje = 40;
 // No se puede modificar el valor de una constante, la siguiente línea genera error
 // porcentaje = 15;
 
-let edad = 25;
+var edad = 25;
 
 // console.log(nombre);
 
 // console.log("apellido1", apellido);
-if(true) {
-    const porcentaje = 15
+if (true) {
+    var _porcentaje = 15;
     // console.log("Constante bloque", porcentaje);
 
-    // apellido = "López"
+    apellido = "López";
     // let edad = 30;
     // console.log("Edad bloque", edad);
 
-    if(true) {
+    if (true) {
 
         // console.log("apellido - segundo bloque", apellido);
     }
-} else {
-
-}
-
+} else {}
 
 // console.log("Constante Raiz:",porcentaje);
 
@@ -38,20 +36,12 @@ if(true) {
 // console.log("apellido2", apellido);
 
 
-
-
-
 /****************************Ejemplos de concatenación e interpolación********************************/
 
 // Concatenación tradicional
-let saludo = "Hola "+nombre+" "+apellido+" ";
+var saludo = "Hola " + nombre + " " + apellido + " ";
 // Concatenación con interpolación
-let saludo2 = `
-    Hola ${nombre} ${apellido}
-    sasdasd
-    asdasd
-    asd
-`
+var saludo2 = "\n    Hola " + nombre + " " + apellido + "\n    sasdasd\n    asdasd\n    asd\n";
 
 // var n1 = prompt("Ingrese el número 1");
 // var n2 = prompt("Ingrese el número 2");
@@ -62,53 +52,54 @@ let saludo2 = `
 // console.log(mensaje);
 /****************************Fin Ejemplos de concatenación e interpolación********************************/
 
-
-
-
 /****************************Ejemplos de Funciones flecha********************************/
 
 // Definición de función con nombre EcmaScript5
 function sumar(num1, num2) {
-    return num1 + num2
+    return num1 + num2;
 }
 
 // Definición de funciones Anónimas
-const sumarAnonima = function (num1, num2) {
-    return num1 + num2
-}
+var sumarAnonima = function sumarAnonima(num1, num2) {
+    return num1 + num2;
+};
 
 //Primera variante de función flecha
-const sumarFlecha1 = (num1, num2) => {
-    return num1 + num2
-}
+var sumarFlecha1 = function sumarFlecha1(num1, num2) {
+    return num1 + num2;
+};
 
 // Segunda variante de función flecha 
-const sumarFlecha2 = (num1, num2) => num1 + num2
+var sumarFlecha2 = function sumarFlecha2(num1, num2) {
+    return num1 + num2;
+};
 
 //Tercera Variante de función flecha con 1 parámetro (Aqui NO es requeridos los paréntesis)
-const saludar = nombre => console.log(`Hola ${nombre}`);
-saludar("Juan Pérez")
+var saludar = function saludar(nombre) {
+    return console.log("Hola " + nombre);
+};
+saludar("Juan Pérez");
 
 // Cuarta variante función flecha sin parámetros (Aqui SI es requeridos los paréntesis)
-const mostrarMensaje = () => console.log("Mensaje función sin parámetros"); 
+var mostrarMensaje = function mostrarMensaje() {
+    return console.log("Mensaje función sin parámetros");
+};
 
-
-mostrarMensaje()
-
+mostrarMensaje();
 
 // Ejemplo solicitado por Alvaro
 // const sumarFlecha3 = (num1, num2) => ({ res1: function1(), res2: funcion2()})
 
 
-let objetoFunciones = {
-    sumarAnonima,
-}
+var objetoFunciones = {
+    sumarAnonima: sumarAnonima
 
+    // var n1 = prompt("Ingrese el número 1");
+    // var n2 = prompt("Ingrese el número 2");
 
-// var n1 = prompt("Ingrese el número 1");
-// var n2 = prompt("Ingrese el número 2");
+    // console.log(`El resultado de la suma es: ${objetoFunciones.sumarAnonima(Number(n1), Number(n2))}`);
 
-// console.log(`El resultado de la suma es: ${objetoFunciones.sumarAnonima(Number(n1), Number(n2))}`);
+    // console.log(objetoFunciones);
+    /****************************Fin Ejemplos de Funciones flecha********************************/
 
-// console.log(objetoFunciones);
-/****************************Fin Ejemplos de Funciones flecha********************************/
+};
